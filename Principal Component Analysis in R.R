ef9d13d529af06a36a6848b7889515c4ecc40278
Syed Izzat Ullah <53023816+SyedIzzatUllah@users.noreply.github.com>
2022-08-15 19:33:27 +0500

    Update Principal Component Analysis in R.R

--- Principal Component Analysis in R.R
+++ Principal Component Analysis in R.R
@@ -43,6 +43,6 @@ ggbiplot(mtcars.pca,ellipse=TRUE,obs.scale = 1, var.scale = 1,  labels=rownames(
          
 spacecar <-c(1000,60,50,500,0,0.5,2.5,0,1,0,0)mtcarsplus <-rbind(mtcars, spacecar)mtcars.countryplus <-c(mtcars.country, "Jupiter")mtcarsplus.pca <-prcomp(mtcarsplus[,c(1:7,10,11)], center = TRUE,scale. = TRUE)ggbiplot(mtcarsplus.pca, obs.scale = 1, var.scale = 1, ellipse = TRUE, circle = FALSE, var.axes=TRUE, labels=c(rownames(mtcars), "spacecar"), groups=mtcars.countryplus)+scale_colour_manual(name="Origin", values= c("forest green", "red3", "violet", "dark blue"))+ggtitle("PCA of mtcars dataset, with extra sample added")+theme_minimal()+theme(legend.position = "bottom")
          
-#Project a new sample onto the original PCA
+#Project a new sample onto the original PCAc 
          
 s.sc <-scale(t(spacecar[c(1:7,10,11)]), center= mtcars.pca$center)s.pred <-s.sc %*% mtcars.pca$rotationmtcars.plusproj.pca <-mtcars.pcamtcars.plusproj.pca$x <-rbind(mtcars.plusproj.pca$x, s.pred)ggbiplot(mtcars.plusproj.pca, obs.scale = 1, var.scale = 1, ellipse = TRUE, circle = FALSE, var.axes=TRUE, labels=c(rownames(mtcars), "spacecar"), groups=mtcars.countryplus)+scale_colour_manual(name="Origin", values= c("forest green", "red3", "violet", "dark blue"))+ggtitle("PCA of mtcars dataset, with extra sample projected")+theme_minimal()+theme(legend.position = "bottom")
